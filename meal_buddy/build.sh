b1c8d7fb6f0b1f3c772ca6abfaa33c196adb9546
Nishant_Totar <70787217+nstotar@users.noreply.github.com>
2025-06-22 04:54:25 +0530

    Auto-load initial data on deploy

--- meal_buddy/build.sh
+++ meal_buddy/build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-set -o errexit
 
-pip install -r requirements.txt && python manage.py collectstatic --noinput && python manage.py migrate
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run migrations
+python manage.py migrate
+
+# Load data
+python manage.py loaddata initial_data.json
